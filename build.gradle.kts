a9515c36ed4215ac283e663d96aa6343a6dbac33
Anders Jändel <sviams@gmail.com>
2021-11-19 15:59:29 +0100

    Update to Kotlin 1.6.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,13 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 group = "org.sviams.aoc.2020"
 version = "1.0-SNAPSHOT"
 
-val kotlin_version = "1.4.20"
-val spek_version = "2.0.9"
-val kluent_version = "1.64"
+val kotlin_version = "1.6.0"
+val spek_version = "2.0.17"
+val kluent_version = "1.68"
 
 
 plugins {
-    kotlin("jvm") version "1.4.20"
+    kotlin("jvm") version "1.6.0"
     application
 }
 
@@ -25,7 +25,7 @@ dependencies {
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:$spek_version")
 
     // spek requires kotlin-reflect, can be omitted if already in the classpath
-    testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.4.20")
+    testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:$kotlin_version")
 }
 
 tasks.test {
